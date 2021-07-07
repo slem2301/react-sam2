2cfd3fed37509b57b36215afd68b10a60ff1f790
slem2301 <anton.vish2301@gmail.com>
2021-07-07 14:31:30 +0300

    footer change 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return(
-        <header className={'header'}>
-            <div className={'container'}>
-                <a href="/" className={'navbar-brand'}>
-                    <img src="https://themified.com/friend-finder/images/logo.png" alt=""/>
-                </a>
-            </div>
-        </header>
+        
     )
 }
 export default Footer;
